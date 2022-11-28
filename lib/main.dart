13fd785dbb557c80b90eba7ef29a9770c2f75dd4
Shu Kuro <shuhei.kuroda@mo-t.com>
2022-11-28 17:17:32 +0900

    Part2 - Step7

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Welcome to Flutter',
-      home: RandomWords(),
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+        ),
+        dividerTheme: const DividerThemeData(
+          color: Colors.green,
+          space: 10,
+          thickness: 10,
+          indent: 0,
+          endIndent: 0
+        )
+      ),
+      home: const RandomWords(),
     );
   }
 }
